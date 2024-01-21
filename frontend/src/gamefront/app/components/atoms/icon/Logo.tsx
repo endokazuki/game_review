3e1920cf00f4a90748aef6b7a468eccc4625887b
endo <worldanalysis1014@gmail.com>
2024-01-22 01:08:34 +0900

    ヘッダーロゴの位置修正

--- frontend/src/gamefront/app/components/atoms/icon/Logo.tsx
+++ frontend/src/gamefront/app/components/atoms/icon/Logo.tsx
@@ -4,9 +4,15 @@ import Image from "next/image";
 
 const Logo: React.FC = () => {
   return (
-    <div className="flex-none sm:flex-1 md:flex-1 lg:flex-1 xl:flex-1">
-      <Link href="/">
-        <Image src="/logo.png" alt="logo" width={200} height={100} />
+    <div className="flex-auto sm:flex-1 md:flex-1 lg:flex-1 xl:flex-1">
+      <Link className="mx-auto" href="/">
+        <Image
+          className="mx-auto"
+          src="/logo.png"
+          alt="logo"
+          width={200}
+          height={100}
+        />
       </Link>
     </div>
   );
--- frontend/src/gamefront/app/components/templates/Header.tsx
+++ frontend/src/gamefront/app/components/templates/Header.tsx
@@ -1,25 +1,24 @@
 "use client";
-import Link from "next/link";
-import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import HamburgerMenu from "../molecules/HamburgerMenu";
 import { menulist } from "../atoms/menu/menuList";
 import MenuDataField from "../atoms/menu/MenuDataField";
 import HamburgerMenuIcon from "../atoms/icon/HamburgerMenuIcon";
-import Logo from "../atoms/icon/logo";
+import Logo from "../atoms/icon/Logo";
 
 const Header: React.FC = () => {
   const [openMenu, setOpenMenu] = useState(false);
   const menuFunction = () => {
     setOpenMenu(!openMenu);
   };
+
   return (
-    <nav className="container mx-auto flex items-center justify-between">
+    <header className="mx-auto flex items-center justify-between bg-gray-800 text-white text-center">
       <Logo />
       {openMenu ? (
         <HamburgerMenu menulist={menulist} menuFunction={menuFunction} />
       ) : undefined}
-      <div className="flex-initial text-[#6F84FF] font-bold m-5 ">
+      <div className="flex-auto text-[#6F84FF] font-bold m-5 ">
         <ul className="md:flex  hidden flex-initial text-left">
           {menulist.map((value, index) => (
             <MenuDataField key={index} menuItem={value} />
@@ -32,7 +31,7 @@ const Header: React.FC = () => {
       >
         <HamburgerMenuIcon />
       </button>
-    </nav>
+    </header>
   );
 };
 export default Header;
